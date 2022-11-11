cd59b89976b9e119db18ccb025d09b18472b3bd5
byht1 <vitalikbyht1@gmail.com>
2022-11-11 20:13:37 +0200

    1

--- src/modules/currency.mjs
+++ src/modules/currency.mjs
@@ -28,12 +28,10 @@ const serverBankDate = async () => {
 const bankDBCurrent = async () => {
   const db = await fs.readFile(bancDB);
   const data = JSON.parse(db);
-  console.log("🚀 ~ data", data);
 
   const date = Date.now();
 
   const isCurrent = date - data.version <= 60 * 1000;
-  console.log("🚀 ~ isCurrent", isCurrent);
 
   return {
     data,
@@ -45,8 +43,6 @@ const exchangeRate = async () => {
   const isValid = await bankDBCurrent();
 
   if (isValid.isCurrent) return isValid.data;
-
-  console.log("next");
 
   return await serverBankDate();
 };
